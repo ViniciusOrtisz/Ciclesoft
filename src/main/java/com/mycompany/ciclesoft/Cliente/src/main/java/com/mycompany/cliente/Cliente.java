d58dbf37fdc3ac6574ecb4ded9df9e821e16cf1d
Maria Isadora <98899367+mmariaisadora@users.noreply.github.com>
2023-03-27 21:56:49 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/ciclesoft/Cliente/src/main/java/com/mycompany/cliente/Cliente.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cliente;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+
+    private int idCliente;
+    private String nome;
+    private String cpf;
+    private int rg;
+    public String endereco;
+    public String bairro;
+    public String estado;
+    public String cep;
+    public int telefone;
+
+    
+    public static void main(String[] args) {
+       System.out.println("Clientes....");
+ }
+    public void excluirCliente(int aExcluirCliente) {
+    }
+
+    public void atualizarCliente(int aAtualizarCliente) {
+    }
+
+    public void cadastrarCliente(int aCadastarCliente) {
+    }
+
+    public void getIdCliente() {
+    }
+
+    public void setNome(String aNome) {
+    }
+
+    public void getNome() {
+    }
+
+    public void setCpf(String aCpf) {
+    }
+
+    public void getCpf() {
+    }
+
+    public void setRg(int aRg) {
+    }
+
+    public void getRg() {
+    }
+
+}
+
