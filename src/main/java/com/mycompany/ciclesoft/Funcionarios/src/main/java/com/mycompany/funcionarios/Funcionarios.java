de3a5d0b8d69526d853fc83f5e457cb4ff22de73
Erick Daniel Teixeira Vier <126730119+ErickDaniel7@users.noreply.github.com>
2023-03-29 19:31:17 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/ciclesoft/Funcionarios/src/main/java/com/mycompany/funcionarios/Funcionarios.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.funcionarios;
+
+/**
+ *
+ * @author erick
+ */
+public class Funcionarios {
+
+      private int idFuncionario;
+      public String nome;
+      public String cargo;
+      public int dataAdmissao;
+      private double salario;
+      public double comissao;
+      public int nCarteiraTrabalho;
+      public int dataNascimento;
+      public String cpf;
+      public String rg;
+      public String telefone;
+      public String email;
+      public String endereco;
+      public String bairro;
+      public String referencia;
+      public String cep;
+      public String cidade;
+      public String estado;
+      
+      public static void main(String[] args) {
+       System.out.println("Funcionarios....");
+       }
+      
+      public void cadastrarFuncionario(int AidFuncionario){
+        idFuncionario = AidFuncionario;
+        
+       }
+      public void atualizarFuncionario(int AidFuncionario){
+        idFuncionario = AidFuncionario;
+        
+       }
+      public void excluirFuncionario(int AidFuncionario){
+        idFuncionario = AidFuncionario;
+        
+       }
+      public void consultarFuncionario(int AidFuncionario){
+        idFuncionario = AidFuncionario;
+        
+       }
+      public void getIdFuncionario(){
+       }
+      public void setIdFuncionario(String aNome){ 
+          
+       }
+      public void getSalario(double aSalario){
+       }
+      public void setSalario(){
+      
+    }
+}
